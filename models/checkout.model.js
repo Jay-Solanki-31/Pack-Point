29779301f95093da9b79978618f88c0f63ec5768
Jay Solanki <jaysolanki1733@gmail.com>
2025-02-24 11:57:21 +0530

    chnage in checkout model

--- models/checkout.model.js
+++ models/checkout.model.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 const OrderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // Add this
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
