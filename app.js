c54b1bd74aa458c0dd8f1c095bf687c93958f575
Jay Solanki <jaysolanki1733@gmail.com>
2025-04-15 13:09:34 +0530

    fix issue

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
-app.use(flash());
+app.use(flash());-
 app.use(verifyJWT)
 app.use(session({
     secret: process.env.SESSION_SECRET,
